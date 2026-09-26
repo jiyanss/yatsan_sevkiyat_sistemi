d2c54fb1435de6a2c301a5471fd7b807e09ef786
jiyanss <80691335+jiyanss@users.noreply.github.com>
2026-09-26 13:02:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3922,7 +3922,6 @@ updateUserUI();
   await load(true);
   if (arsivDahil && !Array.isArray(arsivCache)) {
     try { arsivCache = await fetchArsiv(); render(); } catch (e) { arsivCache = []; }
-  }
   startLiveSync();
   otomatikGunlukYedek();
 })();
